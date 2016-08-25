b3aff63e97d7302d107800f0cb441b2a1294a0af
wangwei <wangwei@jsdttec.com>
2016-08-25 18:22:57 +0800

    add project

---
+++ ChartModule/ChartModule.h
@@ -0,0 +1,19 @@
+//
+//  ChartModule.h
+//  ChartModule
+//
+//  Created by wangwei on 16/8/25.
+//  Copyright © 2016年 JSDT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChartModule.
+FOUNDATION_EXPORT double ChartModuleVersionNumber;
+
+//! Project version string for ChartModule.
+FOUNDATION_EXPORT const unsigned char ChartModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChartModule/PublicHeader.h>
+
+
